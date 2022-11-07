9a6833402cec8197288c1a30a44b9fe987e35f7d
sharonye0 <86781708+sharonye0@users.noreply.github.com>
2022-11-07 11:10:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const API_URL =
    "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=91a2e8830a05d779438a6eaba319c24b&page=1";
 
+
+async function getMovies(url) {
+   const res = await fetch(url);
+   const data = await res.json();
+   showMovies(data.results);
+}
+
+
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 const SEARCH_URL = `http://api.themoviedb.org/3/search/movie?api_key=91a2e8830a05d779438a6eaba319c24b&query="`;
 const cardContainer = document.querySelector(".movie-cards");
 const searchInput = document.getElementById("search");
 const form = document.getElementById("form");
+
 
 function generateMovieMarkup(poster, title, rating, color, overview) {
    return `
@@ -45,12 +54,6 @@ function showMovies(data) {
          generateMovieMarkup(image, title, votes, color, overview)
       );
    });
-}
-
-async function getMovies(url) {
-   const res = await fetch(url);
-   const data = await res.json();
-   showMovies(data.results);
 }
 
 getMovies(API_URL);
